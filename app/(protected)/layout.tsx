2e0baf033cd2af9a4f5925ddab69cec33a8de54b
Nicole <26001237-nicole.natterings@users.noreply.gitlab.com>
2025-07-09 13:45:58 -0400

    Actually protect routes in (protected)

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,15 @@
+import {ReactNode} from "react";
+import {createClient} from "@/utils/supabase/server";
+import {redirect, RedirectType} from "next/navigation";
+
+export default async function Layout({children}: {children: ReactNode}) {
+    console.log("Protected route")
+    const supabase = await createClient();
+    const { data: { user }, error } = await supabase.auth.getUser();
+    if (!user || error) {
+        redirect("/sign-in", RedirectType.replace)
+    }
+    return (
+        <>{children}</>
+    )
+}
