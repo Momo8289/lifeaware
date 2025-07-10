aa93919e85cc2d4cab7fa6fac57ef669056d4367
Nicole <26001237-nicole.natterings@users.noreply.gitlab.com>
2025-07-10 18:53:00 -0400

    see last commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    env: {
+        NEXT_PUBLIC_SITE_URL: process.env?.NEXT_PUBLIC_SITE_URL ??
+        process.env?.NEXT_PUBLIC_VERCEL_URL ??
+        process.env?.NEXT_PUBLIC_VERCEL_BRANCH_URL ??
+        process.env?.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+    }
 };
 
 export default nextConfig;
