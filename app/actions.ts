d732c3b77ad6ee816d64b3706b3d5efd120c33b4
Nicole <26001237-nicole.natterings@users.noreply.gitlab.com>
2025-07-13 22:28:24 -0400

    Sorted out password reset links and added messages

--- app/actions.ts
+++ app/actions.ts
@@ -69,7 +69,6 @@ export const forgotPasswordAction = async (formData: FormData) => {
   // Use Supabase's built-in password reset flow
   // The user will get an email with a link to reset their password
   const url = `${getURL()}reset-password`
-  console.log(url)
   const { error } = await supabase.auth.resetPasswordForEmail(email, {
     // This will direct users back to your app after they set their password in Supabase's UI
     redirectTo: url,
