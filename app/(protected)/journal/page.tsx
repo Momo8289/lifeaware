8f0336332c3f8cee7261caeff4f052a09442a468
RaynaFD <farr0254@algonquinlive.com>
2025-07-28 18:49:31 -0400

    recreated file/folder struct of existing features

---
+++ app/(protected)/journal/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function JournalPage() {
+  return (
+    <div className="container py-6">
+      <h1 className="text-3xl font-bold">My Journal</h1>
+    </div>
+  );
+}
